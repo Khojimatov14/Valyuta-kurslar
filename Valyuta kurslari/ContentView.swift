f1b5efb49f3ebb0021954ee766b3c1f7dea9a510
Khojimatov Anvarjon <khojimatov14@gmail.com>
2022-06-05 12:12:10 +0900

    Initial Commit

---
+++ Valyuta kurslari/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Valyuta kurslari
+//
+//  Created by Anvarjon Khojimatov on 2022/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Valyuta kurslari/Valyuta_kurslariApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valyuta_kurslariApp.swift
+//  Valyuta kurslari
+//
+//  Created by Anvarjon Khojimatov on 2022/06/05.
+//
+
+import SwiftUI
+
+@main
+struct Valyuta_kurslariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
